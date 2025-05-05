d44f50d5034c6a7e52e5d80b5ade0294e5aa488b
akhileshsoniits <77585998+akhileshsoniits@users.noreply.github.com>
2025-05-05 23:35:58 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -6,4 +6,5 @@ docker docker pull akhileshsoni/sample-python-flask-project
 
 # Run the Docker image as a container
 #commitmonday
+
 docker run -d -p 5000:5000 akhileshsoni/sample-python-flask-project
